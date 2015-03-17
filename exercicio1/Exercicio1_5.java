5596853b76f48e94ebe86df529af349fbc8fcb1e
Giovanni Pierroti de Andrade <gpgpierroti@gmail.com>
2015-03-17 11:00:08 -0300

    Create Exercicio1_5.java
    Exercício 5 de atividade desenvolvida em sala no dia 17/03/2015.

---
+++ exercicio1/Exercicio1_5.java
@@ -0,0 +1,17 @@
+public class Exercicio1_5{
+	public static void main(String [] args){
+
+		double a = 45;
+		double b = 20;
+		double c = 3;
+		double soma = a+b+c;
+		double multiplicacao = a*b*c;
+		double divisao = a/b/c;
+		double subtracao = a-b-c;
+
+		System.out.println("O valor da soma foi: " + soma);
+		System.out.println("O valor da multiplicacao foi: " + multiplicacao);
+		System.out.println("O valor da divisao foi: " + divisao);
+		System.out.println("O valor da subtracao foi: " + subtracao);
+	}
+}
