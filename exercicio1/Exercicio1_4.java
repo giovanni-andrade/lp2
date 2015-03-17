44f3ede19f04943085541494ee3a81e9df6fd92f
Giovanni Pierroti de Andrade <gpgpierroti@gmail.com>
2015-03-17 10:56:25 -0300

    Create Exercicio1_4.java
    Exercício de atividade desenvolvida em sala, no dia 17/03/2015.

---
+++ exercicio1/Exercicio1_4.java
@@ -0,0 +1,17 @@
+public class Exercicio1_4{
+	public static void main(String [] args){
+
+		float a = 45;
+		float b = 20;
+		float c = 3;
+		float soma = a+b+c;
+		float multiplicacao = a*b*c;
+		float divisao = a/b/c;
+		float subtracao = a-b-c;
+
+		System.out.println("O valor da soma foi: " + soma);
+		System.out.println("O valor da multiplicacao foi: " + multiplicacao);
+		System.out.println("O valor da divisao foi: " + divisao);
+		System.out.println("O valor da subtracao foi: " + subtracao);
+	}
+}
