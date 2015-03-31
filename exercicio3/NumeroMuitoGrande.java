808df7b4f4d339746fc6b6502e5f6e1051f33de7
Giovanni Pierroti de Andrade <gpgpierroti@gmail.com>
2015-03-31 12:53:39 -0300

    Update NumeroMuitoGrande.java
    Exercício 2 da atividade 3, com aperfeiçoamentos.

--- exercicio3/NumeroMuitoGrande.java
+++ exercicio3/NumeroMuitoGrande.java
@@ -13,16 +13,26 @@ public class NumeroMuitoGrande {
 
         System.out.println("Informe o primeiro número: ");
         String x = input.next();
+		int tamanhox = x.length();
         for (int i = 0; i <= 29; i++) {
-            char caracter = x.charAt(i);
-            intx[i] = Character.getNumericValue(caracter);
+			if ((i+1)<=tamanhox){
+				char caracter = x.charAt(i);
+            	intx[i] = Character.getNumericValue(caracter);
+			}else{
+				intx[i]=0;
+			}
         }
 
         System.out.println("Informe o segundo número: ");
         String y = input.next();
+		int tamanhoy = y.length();
         for (int i = 0; i <= 29; i++) {
-            char caracter = y.charAt(i);
-            inty[i] = Character.getNumericValue(caracter);
+            if ((i+1)<=tamanhoy){
+				char caracter = y.charAt(i);
+            	inty[i] = Character.getNumericValue(caracter);
+			}else{
+				inty[i]=0;
+			}
         }
     }
 
@@ -42,30 +52,32 @@ public class NumeroMuitoGrande {
     }
 
     public void multiplicar() {
-        int k = 0, aux, aux2 = 0, aux3 = 0;
-        int[] intaux = new int[30];
+        int k = 0, aux, aux2 = 0, aux3 = 0, m=0;
+        int[] intaux = new int[61];
         for (int i = 29; i >= 0; i--) {
             for (int j = 29; j >= 0; j--) {
+				k=0;
                 aux = (intx[i] * inty[j]) + aux2;
                 if (aux < 10) {
-                    intaux[k] = aux;
+                    intaux[k+m] = aux;
                     aux2 = 0;
                 } else {
-                    intaux[k] = aux % 10;
+                    intaux[k+m] = aux % 10;
                     aux2 = aux / 10;
                 }
-                k = k + 1;
-                for (int l = 0; l < 60; l++) {
-                    aux = intaux[l] + intmult[l] + aux3;
-                    if (aux < 10) {
-                        intmult[i] = aux;
-                        aux3 = 0;
-                    } else {
-                        intmult[i] = aux % 10;
-                        aux3 = 1;
-                    }
-                }
+				k = k + 1;
             }
+            for (int l = 0; l <= 60; l++) {
+                 aux = intaux[l] + intmult[l] + aux3;
+                 if (aux < 10) {
+                     intmult[l] = aux;
+                     aux3 = 0;
+                 } else {
+                 intmult[l] = aux % 10;
+                     aux3 = 1;
+                 }
+            }
+			m=m+1;
         }
     }
 
@@ -74,13 +86,13 @@ public class NumeroMuitoGrande {
         int a = 0, i = 61;
         while (a == 0) {
             i = i - 1;
-            if (intmult[i] == 0) {
-                a = 0;
-            } else {
+            if (intmult[i] != 0) {
                 a = 1;
+            } else {
+                a = 0;
             }
         }
-        for (int j = i; j <= 0; j--) {
+        for (int j = i; j >= 0; j--) {
             System.out.print(intmult[j]);
         }
 
@@ -95,7 +107,7 @@ public class NumeroMuitoGrande {
                 a = 1;
             }
         }
-        for (int j = i; j <= 30; j--) {
+        for (int j = i; j <= 30; j++) {
             System.out.print(intsoma[j]);
         }
     }
@@ -103,8 +115,8 @@ public class NumeroMuitoGrande {
     public static void main(String[] args) {
         NumeroMuitoGrande g1 = new NumeroMuitoGrande();
         g1.lerNumero();
-        g1.imprimir();
         g1.multiplicar();
         g1.somar();
+		g1.imprimir();
     }
 }
